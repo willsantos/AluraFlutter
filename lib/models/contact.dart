73dc94d19ce1e10ecf6bcd541f4595dbd411319f
Wilson Santos <contato@wilsonsantos.net>
2021-03-31 17:15:01 -0300

    :sparkles:  feat: Add Model Contact

---
+++ lib/models/contact.dart
@@ -0,0 +1,16 @@
+class Contact {
+  final String name;
+  final int account;
+
+  Contact(
+    this.name,
+    this.account,
+  );
+
+  @override
+  String toString() {
+    return 'Contact{name: $name, account: $account}';
+  }
+
+
+}
