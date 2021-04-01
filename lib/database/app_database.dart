bf99948731c6e9c3b5328339cc5f489d1ecb62ca
Wilson Santos <contato@wilsonsantos.net>
2021-04-01 16:36:13 -0300

    :recycle: refactor:Refactor createDatabse func to async

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -2,10 +2,11 @@ import 'package:bytebank/models/contact.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-Future<Database> createDatabase() {
-  return getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'bytebank.db');
-    return openDatabase(path, onCreate: (db, version) {
+Future<Database> createDatabase() async {
+  final String dbPath = await getDatabasesPath();
+  final String path = join(dbPath, 'bytebank.db');
+
+  return openDatabase(path, onCreate: (db, version) {
       db.execute('CREATE TABLE contacts('
           'id INTEGER PRIMARY KEY, '
           'name TEXT, '
@@ -13,7 +14,7 @@ Future<Database> createDatabase() {
     }, version: 1,
     // onDowngrade: onDatabaseDowngradeDelete,
     );
-  });
+
 }
 
 Future<int>save(Contact contact) {
