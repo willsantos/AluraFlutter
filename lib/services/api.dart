8b06be8ca7f92ed70dce02b50e53623e0853db7f
Wilson Santos <contato@wilsonsantos.net>
2021-04-30 10:40:30 -0300

    baguncei tudo

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,15 +1,10 @@
-
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 import 'interceptors/logging_interceptor.dart';
-
-
 
 final Client client = HttpClientWithInterceptor.build(
   interceptors: [LoggingInterceptor()],
 );
 
 const String baseUrl = '2f42a37ec63a.ngrok.io';
-
-
