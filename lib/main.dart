0d275eca9b3e55041a5e8bf2cb0705e4231e4d33
Wilson Santos <contato@wilsonsantos.net>
2021-12-13 13:12:10 -0300

    :wrench: fix: fix dependencies
    Upgrade e fix changes on DotEnv package

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:bytebank/screens/dashboard/dashboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 // import 'package:sqflite/sqflite.dart';
 
 Future main() async {
-  await DotEnv.load(fileName: '.env');
+  await dotenv.load(fileName: '.env');
+
   runApp(ByteBank());
 }
 
