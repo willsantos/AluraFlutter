10ae09e3d671789982b64de9faca585666585b11
Wilson Santos <contato@wilsonsantos.net>
2021-12-18 13:20:55 -0300

    :white_check_mark: test:Add test check display main image in Dashboard
    Remove example test
    Add test: Should display the main image when the Dashboard is open

---
+++ test/dashboard_widget_test.dart
@@ -0,0 +1,12 @@
+import 'package:bytebank/screens/dashboard/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Should display the main image when the Dashboard is open',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final mainImage = find.byType(Image);
+    expect(mainImage, findsOneWidget);
+  });
+}
