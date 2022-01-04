59f11ee67fbbfa21dd265279214a0a5de806259d
Wilson Santos <contato@wilsonsantos.net>
2022-01-04 09:56:23 -0300

    :recycle: refactor(tests):Add object comparison to Contact class
    Add object comparison to Contact class ny Name and AccountNumber

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -14,13 +14,24 @@ class Contact {
     return 'Contact{id:$id,name: $name, account: $account}';
   }
 
-  Contact.fromJson(Map<String,dynamic>json) :
-      id = json['id'],
-      name = json['name'],
-      account = json['accountNumber'];
+  Contact.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        account = json['accountNumber'];
 
-  Map<String,dynamic>toJson() =>{
-    'name': name,
-    'accountNumber':account,
-  };
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'accountNumber': account,
+      };
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Contact &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          account == other.account;
+
+  @override
+  int get hashCode => name.hashCode ^ account.hashCode;
 }
