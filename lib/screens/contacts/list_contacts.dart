9bc87a81a7d9db26b6234c36088f04cbb79814f4
Wilson Santos <contato@wilsonsantos.net>
2022-01-04 11:33:19 -0300

    :sparkles: Finish class 04
    Refactor ContactItem to public
    Check transaction Form with mock data

--- lib/screens/contacts/list_contacts.dart
+++ lib/screens/contacts/list_contacts.dart
@@ -38,7 +38,7 @@ class _ContactsListState extends State<ContactsList> {
                 return ListView.builder(
                   itemBuilder: (context, index) {
                     final Contact contact = contacts[index];
-                    return _ContactItem(contact, onClick: () {
+                    return ContactItem(contact, onClick: () {
                       Navigator.of(context).push(
                         MaterialPageRoute(
                           builder: (context) => TransactionForm(contact),
@@ -69,11 +69,11 @@ class _ContactsListState extends State<ContactsList> {
   }
 }
 
-class _ContactItem extends StatelessWidget {
+class ContactItem extends StatelessWidget {
   final Contact contact;
   final Function onClick;
 
-  _ContactItem(
+  ContactItem(
     this.contact, {
     @required this.onClick,
   });
--- test/flows/transfer_flow.dart
+++ test/flows/transfer_flow.dart
@@ -1,6 +1,8 @@
 import 'package:bytebank/main.dart';
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contacts/list_contacts.dart';
 import 'package:bytebank/screens/dashboard/dashboard.dart';
+import 'package:bytebank/screens/transactions/transaction_form.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -16,6 +18,10 @@ void main() {
     final dashboard = find.byType(Dashboard);
     expect(dashboard, findsOneWidget);
 
+    when(mockContactDao.findAll()).thenAnswer((realInvocation) async {
+      return [Contact(0, 'Will', 1000)];
+    });
+
     await clickOnTheTransferFeatureItem(tester);
     await pumpAndSettleFix(tester);
 
@@ -23,5 +29,19 @@ void main() {
     expect(contactsList, findsOneWidget);
 
     verify(mockContactDao.findAll()).called(1);
+
+    final contactItem = find.byWidgetPredicate((widget) {
+      if (widget is ContactItem) {
+        return widget.contact.name == 'Will' && widget.contact.account == 1000;
+      }
+      return false;
+    });
+    expect(contactItem, findsOneWidget);
+
+    await tester.tap(contactItem);
+    await pumpAndSettleFix(tester);
+
+    final transactionForm = find.byType(TransactionForm);
+    expect(transactionForm, findsOneWidget);
   });
 }
